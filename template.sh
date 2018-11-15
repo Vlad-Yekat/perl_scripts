ad46fdee16c5540da013c98fe26cb13140ebef59
vlad <vladislavi@mail.ru>
2018-11-15 17:41:00 +0600

    go

---
+++ template.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+#https://en.wikipedia.org/wiki/Unix_philosophy
+
+use strict;
+use warnings;
+use 5.016; # пример, say
+
+
+# считываем данные с STDIN, пока они не закончатся
+while (my $line = <>) {
+    #
+}
+
+# какие то манипуляции с входными данными
+# сюда встраивается ваш код
+
+# вывод на STDOUT
+print "...";
+say "...";
